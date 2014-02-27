8abf21f3971e7cd856f32059d3a64085ce5c2a1b
Wardell Bellanger <wbellanger@paypal.com>
2014-02-27 10:25:25 -0600

    refactor location calls

--- ten-twenty/routes/location.js
+++ ten-twenty/routes/location.js
@@ -1,35 +1,29 @@
 module.exports = function(app) {
 
-	var http = require("http");
 	var request = require("request");
 
 	getLocationInfo = function (req, res) {
-		request("http://ipinfo.io/", function(error, response, body) {
-			console.log(body);
-			res.send(body);
-		});
+		execute(res, "http://ipinfo.io/");
 	};
 
 	getGeoLocationInfo = function (req, res) {
-		request("http://ipinfo.io/geo/", function(error, response, body) {
-			console.log(body);
-			res.send(body);
-		});
+		execute(res, "http://ipinfo.io/geo/");
 	};
 
 	getCityInfo = function (req, res) {
-                request("http://ipinfo.io/city/", function(error, response, body) {
-                        console.log(body);
-                        res.send(body);
-                });
+		execute(res, "http://ipinfo.io/city/");
 	};
 
 	getOrgInfo = function (req, res) {
-                request("http://ipinfo.io/org/", function(error, response, body) {
-                        console.log(body);
-                        res.send(body);
-                });
+		execute(res, "http://ipinfo.io/org/");
 	};
+
+	function execute(res, url) {
+	    request(url, function(error, response, body) {
+	            console.log(body);
+	            res.send(body);
+	    });
+	}
 
 	app.get('/loc', getLocationInfo);
 	app.get('/loc/geo', getGeoLocationInfo);
